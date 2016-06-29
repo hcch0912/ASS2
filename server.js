f176d13e2b8cc0cb7c81a9d44613415b62fbe788
veronicachen0912 <veroniachen0912@gmail.com>
2016-06-29 17:02:46 +0900

    :wq

--- server.js
+++ server.js
@@ -5,15 +5,13 @@ var path = require('path');
 var handlebars = require('express-handlebars');
 var bodyParser = require('body-parser');
 var session = require('express-session');
-var dotenv = require('dotenv');
-var pg = require('pg');
+
 var app = express();
 
-//client id and client secret here, taken from .env (which you need to create)
-dotenv.load();
+
 
 //connect to database
-var conString = process.env.DATABASE_CONNECTION_URL;
+
 
 //Configures the Template engine
 app.engine('html', handlebars({ defaultLayout: 'layout', extname: '.html' }));
@@ -27,48 +25,15 @@ app.use(session({ secret: 'keyboard cat',
                   resave: true}));
 
 //set environment ports and start application
-app.set('port', process.env.PORT || 3000);
+app.set('port',3000);
 
 //routes
 app.get('/', function(req, res){
-  var locationData = require('./locations.json');
-  res.render('index', locationData);
+  
+  res.render('index');
 });
 
 
-app.get('')
-
-
-app.get('/delphidata', function (req, res) {
- var results=[];
-
-    pg.connect(conString, function(err, client, done) {
-        // Handle connection errors
-        if(err) {
-          done();
-          console.log(err);
-          return res.status(500).json({ success: false, data: err});
-        }
-
-        // SQL Query > Select Data
-        var query = client.query("SELECT * FROM cogs121_16_raw.hhsa_san_diego_demographics_rental_statistics_2012");
-
-        // Stream results back one row at a time
-        query.on('row', function(row) {
-            results.push(row);
-        });
-
-        // After all data is returned, close connection and return results
-        query.on('end', function() {
-            done();
-            return res.json(results);
-        });
-
-    });
-
-
-  return { delphidata: "No data present." }
-});
 
 
 
